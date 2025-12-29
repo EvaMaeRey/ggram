16efd4c56d7e01fbc224b33ac5f852288e5ac017
iamyannc <djangoworldwide@gmail.com>
2025-12-28 23:09:08 -0400

    refactor to ggram

--- R/ggram.R
+++ R/ggram.R
@@ -1,94 +1,119 @@
-#' @export
-ggram <- function(title = NULL, widths = c(1,1), ...){
-  
+
+# INTERNAL FUNCTIONS ------------------------------------------------------
+
+## History ----
+save_history <- function(){
   temp <- tempfile()
   savehistory(file = temp)
+  temp # return temp for exported functions
+}
+
+
+## Plot ----
+code_plot <- function(temp){
+  code_file_to_code_df(temp) |>
+    code_df_to_code_plot()
+
+}
+
+# patchwork + gt -- Shared in ggram_df_output and ggram_tp_output
+plot_patch_gt <- function(code_plot,
+                          output,
+                          widths = widths,
+                          title = title,
+                          ...
+){
+  patchwork::free(code_plot) + gt::gt(output) + patchwork::plot_layout(widths = widths) +
+    patchwork::plot_annotation(title = title, ...)
+}
+
+# theme -- shared across all 4
+shared_theme <- function(clr = "black", lwd = 0.05){
+  theme(plot.background = element_rect(colour = clr, linewidth = lwd))
+}
+
+
+## Code ----
+code_readLines <- function(temp){
+  readLines(temp) |>
+    grep(pattern = "ggram", x = _,
+         invert = TRUE, value = TRUE) |>
+    paste(collapse = "\n")
+}
+
+eval_code <- function(code){
+  eval(parse(text = code))
+}
+
+# EXPORTED FUNCTIONS ------------------------------------------------------
+
+#' @export
+ggram <- function(title = NULL, widths = c(1,1), ...){
+
+  temp <- save_history()
 
   plot <- last_plot()
-  
-  code_plot <- code_file_to_code_df(temp) |>
-    code_df_to_code_plot()
-  
-  
+
+  code_plot <- code_plot(temp)
+
   patchwork::free(code_plot) + plot + patchwork::plot_layout(widths = widths) +
-    patchwork::plot_annotation(title = title, ...) & 
-    theme(plot.background = element_rect(colour = "black", linewidth = .05))
-  
+  patchwork::plot_annotation(title = title, ...) &
+  shared_theme()
+
 }
 
 #' @export
 ggram_df_output <- function(title = NULL, widths = c(1,1), ...){
-  
-  temp <- tempfile()
-  savehistory(file = temp)
 
-  readLines(temp)[!stringr::str_detect(readLines(temp), "ggram")] |>  
-  paste(collapse = "\n") ->
-    code
-  
-  code_plot <- code_file_to_code_df(temp) |>
-    code_df_to_code_plot()
-  
-  eval(parse(text = code)) -> output
-  
-  
-  patchwork::free(code_plot) + gt::gt(output) + patchwork::plot_layout(widths = widths) +
-  patchwork::plot_annotation(title = title, ...) & 
-  theme(plot.background = element_rect(colour = "black", linewidth = .05))
-    
+  temp <- save_history()
+
+  code <- code_readLines(temp)
+
+  code_plot <- code_plot(temp)
+
+  output <- eval_code(code)
+
+  plot_patch_gt(code_plot, output, widths, title) & shared_theme()
+
 }
 
 #' @export
 ggram_tp_output <- function(title = NULL, widths = c(1,1), ...){
-  
-  temp <- tempfile()
-  savehistory(file = temp)
 
-  readLines(temp)[!stringr::str_detect(readLines(temp), "ggram")] |>  
-  paste(collapse = "\n") ->
-    code
-  
-  code_plot <- code_file_to_code_df(temp) |>
-    code_df_to_code_plot()
-  
-  eval(parse(text = code)) |> tidypivot::collect() -> output
-  
-  
-  patchwork::free(code_plot) + gt::gt(output) + patchwork::plot_layout(widths = widths) +
-  patchwork::plot_annotation(title = title, ...) & 
-  theme(plot.background = element_rect(colour = "black", linewidth = .05))
-    
+  temp <- save_history()
+
+  code <- code_readLines(temp)
+
+  code_plot <- code_plot(temp)
+
+  output <- eval_code(code) |> dplyr::collect()
+
+  plot_patch_gt(code_plot, output, widths, title) & shared_theme()
 }
-
 
 #' @export
 ggram_text_output <- function(title = NULL, widths = c(1,1), ...){
-  
-  temp <- tempfile()
-  savehistory(file = temp)
 
-  readLines(temp)[!stringr::str_detect(readLines(temp), "ggram")] |>  
-  paste(collapse = "\n") ->
-    code
-  
-  code_plot <- code_file_to_code_df(temp) |>
-    code_df_to_code_plot()
-  
-  capture.output(eval(parse(text = code))) -> output
-  
-  output |>
+  temp <- save_history()
+
+  code <- code_readLines(temp)
+
+  code_plot <- code_plot(temp)
+
+  output <- eval_code(code) |> utils::capture.output()
+
+  text_output_plot <- output |>
     data.frame(code = _) |>
       dplyr::mutate(row_number = dplyr::row_number()) |>
-      ggplot() + 
-      aes(x = 1, 
-          y = row_number, 
+      ggplot() +
+      aes(x = 1,
+          y = row_number,
           label = code) +
       geom_text(hjust = 0, family = "mono") +
-    stamp_notebook() ->
-    text_output_plot
-  
+    stamp_notebook()
+
   patchwork::free(code_plot) + text_output_plot + patchwork::plot_layout(widths = widths) +
-  patchwork::plot_annotation(title = title, ...) & 
-  theme(plot.background = element_rect(colour = "black", linewidth = .05))
-    
+  patchwork::plot_annotation(title = title, ...) &
+  shared_theme()
+
 }
