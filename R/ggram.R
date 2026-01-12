a2fbd0fa39d05f6f23f219dd4702b25bb065f6e3
iamyannc <djangoworldwide@gmail.com>
2026-01-11 23:41:22 -0400

    relocating ... argument for better design control

--- R/ggram.R
+++ R/ggram.R
@@ -1,11 +1,11 @@
 #' @export
 ggram <- function(title = NULL, widths = c(1,1), code = NULL, style = stamp_notebook(), output_plot = NULL, ...){
-  
+
   code <- get_code()
-  code_plot <- specify_code_plot(code, style = style)
+  code_plot <- specify_code_plot(code, style = style,...)
   output <- eval(parse(text = code))
   output_plot <- output
-  
-  patch_code_and_output(code_plot, output_plot, widths, title, ...)
-  
+
+  patch_code_and_output(code_plot, output_plot, widths, title)
+
 }
